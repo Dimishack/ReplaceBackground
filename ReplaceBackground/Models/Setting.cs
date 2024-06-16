0076808f89c031a237e861c535fc4832ae9b6596
Dimishack <dimishak.git@mail.ru>
2024-06-16 21:54:31 +0300

    Добавление пакетов Behaviors и чтение файлов json. Добавление файла с настройками

---
+++ ReplaceBackground/Models/Setting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReplaceBackground.Models
+{
+    internal class Setting
+    {
+        public DateTime DateReplaced { get; set; } = DateTime.Today;
+        public string Interval { get; set; } = "Месяц";
+    }
+}
