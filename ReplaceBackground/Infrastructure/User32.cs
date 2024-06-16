0cf2bf71ed29a64eb4362594ec6b1a178b9ff857
Dimishack <dimishak.git@mail.ru>
2024-06-16 21:56:52 +0300

    user32 с методом изменеия фона

---
+++ ReplaceBackground/Infrastructure/User32.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+namespace ReplaceBackground.Infrastructure
+{
+    internal class User32
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern int SystemParametersInfo
+            (int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        public static void SetWallpaper(string path, int style, int title)
+        {
+            using (var key = Registry.CurrentUser.OpenSubKey("Control Panel\\Desktop", true))
+            {
+                key.SetValue("WallpaperStyle", style.ToString());
+                key.SetValue("TileWallpaper", title.ToString());
+                SystemParametersInfo(20, 0, path, 0x01 | 0x02);
+            }
+        }
+    }
+}
